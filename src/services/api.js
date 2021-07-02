425664094bd6c8372771bf30cabbc969b4730e20
Leandro Custodio <leandro.arthurcustodio@gmail.com>
2021-07-01 23:46:48 -0300

    teste

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,6 @@
 import axios from "axios";
-console.log("process", process.env)
-console.log("GCLOUD_PROJECT", process.env.GCLOUD_PROJECT)
-console.log("FIREBASE_CONFIG", process.env.FIREBASE_CONFIG)
 const BASE_URL = ""
-if (process.env.GCLOUD_PROJECT === 'here-present-5e704') {
+if (process.env.NODE_ENV === "production") {
   BASE_URL = "https://herepresent.herokuapp.com"
 } else {
   BASE_URL = process.env.REACT_APP_WEATHER_BASE_URL;
