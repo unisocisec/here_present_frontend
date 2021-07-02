119b3a9813ec0ef0331558de8f6dd11fa404eeb0
Leandro Custodio <leandro.arthurcustodio@gmail.com>
2021-07-01 23:20:17 -0300

    BASE_URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,8 @@
 import axios from "axios";
-// const functions = require('firebase-functions');
-// const config = functions.config();
-// // Porting envs from firebase config
-// for (const key in config.env){
-//   process.env[key.toUpperCase()] = config.env[key];
-// }
-
-const BASE_URL = process.env.REACT_APP_WEATHER_BASE_URL;
+if (process.env.GCLOUD_PROJECT === 'here-present-5e704') {
+  const BASE_URL = "https://herepresent.herokuapp.com"
+} else {
+  const BASE_URL = process.env.REACT_APP_WEATHER_BASE_URL;
+}
 const api = axios.create({baseURL: BASE_URL})
 export default api
