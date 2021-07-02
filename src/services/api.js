2d4caf53d1ba67d95741a0fc93d159eef677bcfc
Leandro Custodio <leandro.arthurcustodio@gmail.com>
2021-07-01 23:35:23 -0300

    GCLOUD_PROJECT

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,12 @@
 import axios from "axios";
+console.log("process", process.env)
+console.log("GCLOUD_PROJECT", process.env.GCLOUD_PROJECT)
+console.log("FIREBASE_CONFIG", process.env.FIREBASE_CONFIG)
+const BASE_URL = ""
 if (process.env.GCLOUD_PROJECT === 'here-present-5e704') {
-  const BASE_URL = "https://herepresent.herokuapp.com"
+  BASE_URL = "https://herepresent.herokuapp.com"
 } else {
-  const BASE_URL = process.env.REACT_APP_WEATHER_BASE_URL;
+  BASE_URL = process.env.REACT_APP_WEATHER_BASE_URL;
 }
 const api = axios.create({baseURL: BASE_URL})
 export default api
