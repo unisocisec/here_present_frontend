7c08e82541277d5f81e9de449596fa69c95e9906
Leandro Custodio <leandro.arthurcustodio@gmail.com>
2021-07-05 21:46:31 -0300

    change icon error answer

--- src/components/StudentReport/index.js
+++ src/components/StudentReport/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Tr, Td } from "@chakra-ui/react";
 import { Icon } from "@chakra-ui/react"
-import { CheckIcon } from '@chakra-ui/icons'
+import { CheckIcon, CloseIcon } from '@chakra-ui/icons'
 
 import './styles.css';
 
@@ -11,7 +11,7 @@ export default function StudentReport({dataKey, name, email, confirmationCode, c
       <Td key={`name_${dataKey}`}>{name}</Td>
       <Td key={`email_${dataKey}`}>{email}</Td>
       <Td key={`confirmationCode_${dataKey}`}>{confirmationCode}</Td>
-      <Td key={`check_${dataKey}`}>{check ? <Icon as={CheckIcon} color="green"/> : <Icon as={CheckIcon} color="red"/>}</Td>
+      <Td key={`check_${dataKey}`}>{check ? <Icon as={CheckIcon} color="green"/> : <Icon as={CloseIcon} color="red"/>}</Td>
     </Tr>
   );
 }
